c8798d67cd81a1eea3ca55ccc43fae78e2947784
MustafaShami <mustafa.shami@ucdenver.edu>
2022-05-05 18:34:54 -0600

    header.css includes some simple styles for the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-      <div className="app">
-        <h1>Dating App using MERN stack</h1>
-      </div>
+      <Header>
+          <div className="app">
+              <h1>Dating App using MERN stack</h1>
+          </div>
+      </Header>
   );
 }
 
