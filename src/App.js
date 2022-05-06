767f9f4a15a5046ccc2702e5009dc0bb25c80729
MustafaShami <mustafa.shami@ucdenver.edu>
2022-05-06 14:48:35 -0600

    tries to add authentication page but wasn't able to

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import './App.css';
 import Header from './components/Header';
 import DatingCards from './components/DatingCards';
 import SwipeButtons from './components/SwipeButtons';
+import Authentication from './components/authentication';
+import {HashRouter, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './stores/store';
+
+
+//Trying to implement authorization
+// function App() {
+//     return (
+//         <div className="app">
+//             <Provider store={store}>
+//                 <HashRouter>
+//                     <div>
+//                         <Header />
+//                         < DatingCards />
+//                         < SwipeButtons />
+//                         <Route exact path="/" render={()=>< DatingCards />}/>
+//                         <Route path="/signin" render={()=><Authentication />}/>
+//                     </div>
+//                 </HashRouter>
+//             </Provider>
+//         </div>
+//     );
+// }
+
+
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
 import IconButton from '@material-ui/core/IconButton'
 import ForumIcon from '@material-ui/icons/Forum'
+import {logoutUser} from "../actions/authActions";
+import {LinkContainer} from 'react-router-bootstrap';
+import {connect} from 'react-redux';
+import {Navbar, Nav} from 'react-bootstrap';
 
-// adding buttons to go to profile and to go to chat
+// maybe add buttons to go to profile and to go to chat
+
+
+// class Header extends Component {
+//     logout() {
+//         this.props.dispatch(logoutUser());
+//     }
+// render() {
+//     return (
+//         <div>
+//             <div className="header">
+//                 <IconButton>
+//                     <PersonIcon fontSize="large" className="header__icon"/>
+//                 </IconButton>
+//                 <img className="header__logo" src="logo192.png" alt="header"/>
+//                 <h1><span className="ribbon-highlight">BTT DATING</span></h1>
+//                 <img className="header__logo" src="logo192.png" alt="header"/>
+//                 <IconButton>
+//                     <ForumIcon fontSize="large" className="header__icon"/>
+//                 </IconButton>
+//             </div>
+//
+//             <div className="headerLogin">
+//                 <Navbar expand="lg" bg="dark" variant="dark">
+//                     <Navbar.Brand>
+//                         Dating App
+//                     </Navbar.Brand>
+//                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+//                     <Navbar.Collapse id="basic-navbar-nav">
+//                         <Nav>
+//                             <LinkContainer to="/dating/cards">
+//                                 <Nav.Link disabled={!this.props.loggedIn}>Singles For You</Nav.Link>
+//                             </LinkContainer>
+//                             <LinkContainer to="/signin">
+//                                 <Nav.Link>{this.props.loggedIn ?
+//                                     <button onClick={this.logout.bind(this)}>Logout</button> : 'Login'}</Nav.Link>
+//                             </LinkContainer>
+//                         </Nav>
+//                     </Navbar.Collapse>
+//                 </Navbar>
+//             </div>
+//         </div>
+//
+//         )
+//     }
+// }
+//
+// const mapStateToProps = state => {
+//     return {
+//         loggedIn : state.auth.loggedIn,
+//         username : state.auth.username,
+//     }
+// }
+//
+// export default connect(mapStateToProps)(Header);
 
 const Header = () => {
+
     return (
         <div className="header">
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon" />
             </IconButton>
             <img className="header__logo" src="logo192.png" alt="header" />
-            <h1> <span class="ribbon-highlight">BTT DATING</span></h1>
+            <h1> <span className="ribbon-highlight">BTT DATING</span></h1>
             <img className="header__logo" src="logo192.png" alt="header" />
             <IconButton>
                 <ForumIcon fontSize="large" className="header__icon" />
             </IconButton>
         </div> )
 }
-
 export default Header;
