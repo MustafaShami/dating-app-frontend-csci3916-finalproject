5e2c474ea2d970a1ca90e736e8175c8a642e0909
MustafaShami <mustafa.shami@ucdenver.edu>
2022-05-06 00:55:38 -0600

    fixed some bugs for got some syntax elements

--- src/components/DatingCards.js
+++ src/components/DatingCards.js
@@ -31,11 +31,12 @@ const DatingCards = () => {
         <div className="datingCards">
             <div className="datingCards__container">
                 {people.map((person) => (
-                    <DatingCard>
+                    <DatingCard
                         className="swipe"
                         key={person.name}
                         preventSwipe={['up', 'down']}
-                        onSwipe={(dir) => swiped(dir, person.name)} >
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)} >
                         <div style={{ backgroundImage: `url(${person.imgUrl})`}} className="card">
                             <h3>{person.name}</h3>
                         </div>
@@ -43,7 +44,6 @@ const DatingCards = () => {
                 ))}
             </div>
         </div>
-
     )
 }
 
