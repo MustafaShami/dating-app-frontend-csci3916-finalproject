2a4464fed259bb0a830f0527fd12108563aeb40b
MustafaShami <mustafa.shami@ucdenver.edu>
2022-05-05 18:40:06 -0600

    //comment (explaining what buttons we have so far

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
 import IconButton from '@material-ui/core/IconButton'
 import ForumIcon from '@material-ui/icons/Forum'
+
+// adding buttons to go to profile and to go to chat
+
 const Header = () => {
     return (
         <div className="header">
